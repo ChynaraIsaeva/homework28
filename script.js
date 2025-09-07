d9de73ec8d38e994424bb6535a7ccd7611c69587
ChynaraIsaeva <isaeva.c.789@gmail.com>
2025-09-07 17:19:16 +0600

    update/ function

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-// function reverseIt(data) {
-//     if (typeof data === 'string' || typeof data === 'number') {
-//       const reversed = String(data).split('').reverse().join('');
-//       return typeof data === 'number' ? Number(reversed) : reversed;
-//     }
-//     return data;
-//   }
+function reverseIt(data) {
+    if (typeof data === 'string' || typeof data === 'number') {
+      const reversed = String(data).split('').reverse().join('');
+      return typeof data === 'number' ? Number(reversed) : reversed;
+    }
+    return data;
+  }
 
-//   console.log(reverseIt("hello"));    // "olleh"
-// console.log(reverseIt(12345));      // 54321
+  console.log(reverseIt("hello"));    // "olleh"
+console.log(reverseIt(12345));      // 54321
 
 
-// function archersReady(archers) {
-//     return archers.length?archers.every(archer=>archer>=5):false;
-// }
+function archersReady(archers) {
+    return archers.length?archers.every(archer=>archer>=5):false;
+}
 
-// archersReady([5, 6, 7, 8])
-// archersReady([5, 3, 6])
+archersReady([5, 6, 7, 8])
+archersReady([5, 3, 6])
 
 
 function generatePairs(n) {
@@ -27,5 +27,5 @@ function generatePairs(n) {
     }
     return result;
   }
-  
+
   console.log(generatePairs(2));
